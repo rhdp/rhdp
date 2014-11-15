89f96f61322a8193b54c5d392d56562433e148f1
edge <sort@programmer.net>
2014-11-15 02:30:09 -0500

    create basic layout

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,36 @@
+var cachedArticles = [],
+	Article = can.Model.extend({
+		render: marked
+	}, {
+		init: function(md) {
+			this.html = Article.render(md);
+		},
+		html: ''
+	}),
+	ArticleView = can.Control.extend({
+		init: function(article) {
+			this.article = article;
+		},
+		article: {}
+	});
+
 $(document).ready(function() {
 	var socket = io.connect(),
 		T = templatizer;
+
+	var header = $('#header'),
+		toolbar = $('#toolbar'),
+		search = $('#search'),
+		topicbar = $('#topicbar'),
+		content = $('#content');
+
+	var loadArticle = function(md) {
+		var a = new Article(md),
+			v = $(T.article(a)).appendTo(content),
+			c = new ArticleView(v, a);
+	};
+
+	loadArticle('# Hello\n## Hello\n ### Hello\n#### Hello\n##### Hello\n###### Hello');
 
 	socket
 		.emit('init', {
